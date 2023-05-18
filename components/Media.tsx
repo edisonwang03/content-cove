5ed86d4a76de01c7b9c8da8c322219c92a9c440d
Edison Wang <edisonwang03@gmail.com>
2023-05-17 22:40:00 -0400

    Finished UI

--- components/Media.tsx
+++ components/Media.tsx
@@ -4,13 +4,18 @@ import ReactMarkdown from "react-markdown";
 
 const Media = () => {
   return (
-    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+    
+    <div className="card card-compact w-96 bg-gray-900 shadow-xl">
       <figure><img src="../images/thumbnail.jpg" alt="Thumbnail" /></figure>
       <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+      <h2 className="card-title truncate overflow-hidden">
+        <span className="inline-block max-w-full truncate">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+      </h2>
+        <p>Placeholder Channel</p>
+        <p>10,000 Views/Viewers</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <div className="badge bg-purple-700 text-white font-bold">Twitch</div>
+          <div className="badge bg-red-700 text-white font-bold">YouTube</div>
         </div>
       </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,35 @@ const Home  = () => {
   return (
     <Layout>
       <h2 className="text-4xl font-extrabold dark:text-white mb-10">Home</h2>
-      <Media></Media>
+      <div className="flex flex-wrap justify-center gap-10">
+        <div className="w-400 flex-none">
+          <Media />
+        </div>
+        <div className="w-400 flex-none">
+          <Media />
+        </div>
+        <div className="w-400 flex-none">
+          <Media />
+        </div>
+        <div className="w-400 flex-none">
+          <Media />
+        </div>
+        <div className="w-400 flex-none">
+          <Media />
+        </div>
+        <div className="w-400 flex-none">
+          <Media />
+        </div>
+        <div className="w-400 flex-none">
+          <Media />
+        </div>
+        <div className="w-400 flex-none">
+          <Media />
+        </div>
+        <div className="w-400 flex-none">
+          <Media />
+        </div>
+      </div>
     </Layout>
   )
 }
